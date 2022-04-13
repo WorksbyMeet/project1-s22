bdd52b0a050e9593211701aa01da8589d0d78a77
Meet Patel <meet@Meets-MacBook-Air.local>
2022-04-13 12:02:51 -0400

    Added part3 start file of my own

---
+++ webserver/part3.py
@@ -0,0 +1,8 @@
+#project1part 3 implement
+import os
+from sqlalchemy import *
+from sqlalchemy.pool import NullPool
+from flask import Flask, request, render_template, g, redirect, Response
+
+tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+app = Flask(__name__, template_folder=tmpl_dir)
