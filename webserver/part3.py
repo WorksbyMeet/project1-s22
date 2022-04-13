90abcde5e9dc1030fbf901700d8eb489e07965f2
Meet Patel <meet@Meets-MacBook-Air.local>
2022-04-13 13:34:15 -0400

    .

--- webserver/part3.py
+++ webserver/part3.py
@@ -49,21 +49,27 @@ def teardown_request(exception):
   except Exception as e:
     pass
 
-@app.route('/')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    usernames = g.conn.execute("SELECT userid FROM users")
+    userids =[]
+    for i in usernames:
+      userids.append(i['userid'])
+    usernames.close()
+
+    if request.method == 'POST':
+        if request.form['username'] != usernames or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('/index'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/index')
 def index():
-  """
-  request is a special object that Flask provides to access web request information:
 
-  request.method:   "GET" or "POST"
-  request.form:     if the browser submitted a form, this contains the data in the form
-  request.args:     dictionary of URL arguments e.g., {a:1, b:2} for http://localhost?a=1&b=2
-
-  See its API: http://flask.pocoo.org/docs/0.10/api/#incoming-request-data
-  """
-
-  # DEBUG: this is debugging code to see what request looks like
   print (request.args)
-
 
   #
   # example of a database query
@@ -74,39 +80,9 @@ def index():
     names.append(result['name'])  # can also be accessed using result[0]
   cursor.close()
 
-  #
-  # Flask uses Jinja templates, which is an extension to HTML where you can
-  # pass data to a template and dynamically generate HTML based on the data
-  # (you can think of it as simple PHP)
-  # documentation: https://realpython.com/blog/python/primer-on-jinja-templating/
-  #
-  # You can see an example template in templates/index.html
-  #
-  # context are the variables that are passed to the template.
-  # for example, "data" key in the context variable defined below will be 
-  # accessible as a variable in index.html:
-  #
-  #     # will print: [u'grace hopper', u'alan turing', u'ada lovelace']
-  #     <div>{{data}}</div>
-  #     
-  #     # creates a <div> tag for each element in data
-  #     # will print: 
-  #     #
-  #     #   <div>grace hopper</div>
-  #     #   <div>alan turing</div>
-  #     #   <div>ada lovelace</div>
-  #     #
-  #     {% for n in data %}
-  #     <div>{{n}}</div>
-  #     {% endfor %}
-  #
+ 
   context = dict(data = names)
 
-
-  #
-  # render_template looks in the templates/ folder for files.
-  # for example, the below file reads template/index.html
-  #
   return render_template("index.html", **context)
 
 
