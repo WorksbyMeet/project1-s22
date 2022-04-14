f11aa719528d8d67ae1ad778a585d456f2bb3b36
Meet Patel <meet@meets-air.myfiosgateway.com>
2022-04-14 16:53:06 -0400

    a

--- webserver/part3.py
+++ webserver/part3.py
@@ -190,7 +190,7 @@ def site():
     description_u.append(i[0])  # can also be accessed using result[0]
   description_unique.close()
 
-  street_unique = g.conn.execute('SELECT street from (SELECT * FROM address INNER JOIN has ON has.aid=address.aid) as t Where name = (%s)',category)
+  street_unique = g.conn.execute('SELECT number,street,city,state,zip from (SELECT * FROM address INNER JOIN has ON has.aid=address.aid) as t Where name = (%s)',category)
   street_u = []
   for i in street_unique:
     street_u.append(i[0])  # can also be accessed using result[0]
